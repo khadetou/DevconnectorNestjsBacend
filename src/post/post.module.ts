cb91f2f7b90ca5bf6c47af861571840b9ca4e029
khadetou <khadetou96@gmail.com>
2022-02-24 10:09:59 +0000

    post module mongoose module config

--- src/post/post.module.ts
+++ src/post/post.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { PostService } from './post.service';
 import { PostController } from './post.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from 'src/auth/auth.module';
+import { Post, PostSchema } from './schemas/post.schema';
 
 @Module({
-  providers: [PostService],
-  controllers: [PostController]
+	imports: [ MongooseModule.forFeature([ { name: Post.name, schema: PostSchema } ]), AuthModule ],
+	providers: [ PostService ],
+	controllers: [ PostController ]
 })
 export class PostModule {}

6b137924d3410f53b2ac14573d5dfbd98f86acc0
khadetou <khadetou96@gmail.com>
2022-02-24 12:43:52 +0000

    cors enabled

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	const logger = new Logger('bootstrap');
 	app.useGlobalPipes(new ValidationPipe());
+	app.enableCors();
 	const port = process.env.PORT || 3000;
 	await app.listen(port);
 	logger.log(`Application is running on: ${await app.getUrl()}`);

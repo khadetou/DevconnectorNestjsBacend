72c3ad794d96aa692509bd2af5068d4cba5db2b0
khadetou <khadetou96@gmail.com>
2022-02-24 10:12:41 +0000

    post model Injection in service

--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
-
+import { InjectModel } from '@nestjs/mongoose';
+import { Post, PostDocument } from './schemas/post.schema';
+import { Model } from 'mongoose';
 @Injectable()
-export class PostService {}
+export class PostService {
+	constructor(@InjectModel(Post.name) private readonly postModel: Model<PostDocument>) {}
+}
